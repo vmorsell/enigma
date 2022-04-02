496d9d5204dac895c34dac5b362a92fa11b9dcf5
Viktor Mörsell <viktor@upper.st>
2022-04-03 00:41:15 +0200

    Add key-string functions

--- enigma/keys.go
+++ enigma/keys.go
@@ -1,5 +1,7 @@
 package enigma
 
+import "fmt"
+
 type Key int
 
 const (
@@ -31,7 +33,7 @@ const (
 	Z
 )
 
-const numKeys = 26
+var values = [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
 func (k Key) Int() int {
 	return int(k)
@@ -40,7 +42,37 @@ func (k Key) Int() int {
 func (k Key) Shift(offset int) Key {
 	new := (k + Key(offset))
 	for new < 0 {
-		new += numKeys
+		new += Key(len(values))
 	}
-	return new % numKeys
+	return new % Key(len(values))
+}
+
+func (k Key) String() string {
+	return values[k]
+}
+
+var errUnknownKey = func(s string) error { return fmt.Errorf("unknown key %s", s) }
+
+func stringToKeys(s string) ([]Key, error) {
+	out := make([]Key, 0, len(s))
+nextRune:
+	for _, r := range s {
+		c := fmt.Sprintf("%c", r)
+		for i, v := range values {
+			if v == c {
+				out = append(out, Key(i))
+				continue nextRune
+			}
+		}
+		return nil, errUnknownKey(s)
+	}
+	return out, nil
+}
+
+func keysToString(keys []Key) string {
+	var out string
+	for _, k := range keys {
+		out += k.String()
+	}
+	return out
 }
--- enigma/keys_test.go
+++ enigma/keys_test.go
@@ -1,6 +1,7 @@
 package enigma
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -51,6 +52,44 @@ func TestShift(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			res := tt.k.Shift(tt.offset)
 			require.Equal(t, tt.res, res)
+		})
+	}
+}
+
+func TestString(t *testing.T) {
+	alphaOffset := 65
+
+	for i := 0; i < len(values); i++ {
+		want := fmt.Sprintf("%c", i+alphaOffset)
+		got := Key(i).String()
+		require.Equal(t, want, got)
+	}
+}
+
+func TestStringToKeys(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		k    []Key
+		err  error
+	}{
+		{
+			name: "not ok - key not found",
+			s:    "Ö",
+			err:  errUnknownKey("Ö"),
+		},
+		{
+			name: "ok",
+			s:    "A",
+			k:    []Key{A},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			keys, err := stringToKeys(tt.s)
+			require.Equal(t, tt.err, err)
+			require.Equal(t, tt.k, keys)
 		})
 	}
 }
